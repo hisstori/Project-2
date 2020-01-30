4fa416e5796a06ff594b6bf9a0d1f9831346f4e0
Hisstori <browncryan@outlook.com>
2020-01-30 00:54:06 -0500

    Added deck of cards.

---
+++ main.js
@@ -0,0 +1,9 @@
+//Start of Logic
+
+let cardDeck = {
+	hearts: ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
+	diamonds: ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
+	club: ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
+	spades: ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
+}
+//Create deck of cards.
