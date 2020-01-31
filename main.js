534de835b911926408ca67d4b21f54de91e18e79
Hisstori <browncryan@outlook.com>
2020-01-31 14:30:15 -0500

    Revised decks

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
 //Start of Logic
-
-let cardDeck = {
-	hearts: ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
-	diamonds: ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
-	club: ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
-	spades: ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
+class Card {
+	constructor(suit, rank, score) {
+	this.suit = ['hearts', 'spades', 'clubs', 'diamonds']
+	this.rank = ['A', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+	this.score = (14, 1, 2, 3, 4, 5, 6 , 7, 8, 9, 10, 11, 12, 13)
 }
-//Create deck of cards.
+};
+
+class Deck {
+   constructor(length, cards, draw) {
+	   suits = ['hearts', 'spades', 'clubs', 'diamonds']
+	   ranks = [1,2,3,4,5,6,7,8,9,10,'J','Q','K','A',]
+   this.length = length
+	   for (i = 0; i < suit.length; i++) {
+		   console.log("Game Created")
+		   for (j = 0; j < rank.length; j++) 
+		   Deck[i+ j * rank.length] = new Card(rank[j],suit[i]); {
+		   console.log(rank[j])
+		   }
+	   }
+	   let draw = (rank, suit)
+   }
+}
+console.log(Deck); 
