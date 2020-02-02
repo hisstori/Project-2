4e395ad1c5f555a8891fa34989422b924597b39d
Hisstori <browncryan@outlook.com>
2020-02-02 01:19:43 -0500

    Deal function completed.

--- war.js
+++ war.js
@@ -1,10 +1,11 @@
 console.log("Welcome to War!");
 
-const suits = ["spades", "clubs", "hearts", "diamonds"]
-const ranks = "A23456789TJQK"
-const deck = new Array(ranks.length * suits.length)
-const playerOneDeck = []
-const playerTwoDeck = []
+const suits = ["spades", "clubs", "hearts", "diamonds"];
+const ranks = "A23456789TJQK";
+const deck = new Array(ranks.length * suits.length);
+let playerOneDeck = [];
+let playerTwoDeck = [];
+const table = [];
 
 let splitDeck = Math.ceil(deck.length / 2)
 
@@ -20,7 +21,7 @@ function buildDeck() {
         deck[j + i * ranks.length] = new Card(ranks[j],suits[i])
     }
     console.log("Prepare for War!")
-    return deck;      
+    return deck;    
 };
 
 function shuffle(deck) {
@@ -35,7 +36,17 @@ function shuffle(deck) {
     }
     return deck;
 };
-// function cardsDealt() {
-//     if (playerOneDeck.length < 0; 
-// }
 
+function deal(deck) {
+        playerOneDeck = deck.splice(0,26);
+        console.log("Player One, has their deck.");
+        playerTwoDeck = deck.splice(0,26);
+        console.log("Player Two, has their deck.");      
+};
+
+function playCard() {
+    if (table.length < 0 || table.length < 1) {
+        playerOneDeck.push(table);
+        playerTwoDeck.push(table);
+    }
+};
